43db80f22b8088d82e2b011ea8da94b6e22a27a3
Sean Ingimarson <singima@clemson.edu>
2023-02-09 12:11:06 -0500

    Built AA folder

---
+++ Anderson_Acceleration/step-77-NOX/CMakeFiles/step-77.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sean/Grad_Research/Anderson_Acceleration/step-77-NOX/step-77.cc" "CMakeFiles/step-77.dir/step-77.cc.o" "gcc" "CMakeFiles/step-77.dir/step-77.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Anderson_Acceleration/step-77-NOX/CMakeFiles/step-77.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step-77.dir/step-77.cc.o"
+  "CMakeFiles/step-77.dir/step-77.cc.o.d"
+  "step-77"
+  "step-77.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step-77.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
